1e5048d55fe2163b456e4c2cc78fa76cdd44f69c
Jonathan <deschuyteneerj@gmail.com>
2022-01-08 15:12:00 +0100

    'v2'

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.behaviors import CoverBehavior
 
+from http_client import HttpClient
 from models import Pizza
+from storage_manager import StorageManager
 
 
 class PizzaWidget(BoxLayout):
@@ -17,19 +19,32 @@ class PizzaWidget(BoxLayout):
 
 class MainWidget(FloatLayout):
     recycleView = ObjectProperty(None)
+    error_str = StringProperty('')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.pizzas = [
+        """self.pizzas = [
             Pizza('4 Formaggi', 'Tomato sauce, mozzarella, taleggio, gorgonzola, parmesan', 11.00, True),
             Pizza('Vegetarian', 'Tomato sauce, mozzarella, peppers, onions, mushrooms', 8.50, True),
             Pizza('Casa', 'Tomato sauce, mozzarella, broccoli, homemade sausage', 11.50, False),
             Pizza('Salmone', 'Tomato sauce, mozzarella, taleggio, gorgonzola, parmesan', 13.99, False)
-        ]
+        ]"""
+
+        HttpClient().get_pizzas(self.on_server_data, self.on_server_error)
 
     def on_parent(self, widget, parent):
-        l = [pizza.get_dictionary() for pizza in self.pizzas]
-        self.recycleView.data = l
+        # l = [pizza.get_dictionary() for pizza in self.pizzas]
+        pizzas_dict = StorageManager().load_data('pizzas')
+        if pizzas_dict:
+            self.recycleView.data = pizzas_dict
+
+    def on_server_data(self, pizzas_dict):
+        self.recycleView.data = pizzas_dict
+        StorageManager().save_data('pizzas', pizzas_dict)
+
+    def on_server_error(self, error):
+        print('ERROR: ' + str(error))
+        self.error_str = 'ERROR: ' + str(error)
 
 
 with open("pizzascr.kv", encoding='utf8') as f:
